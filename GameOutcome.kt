01cbd226e448b395212bca85ade51587aea8a6d3
ChenZhang1999 <78033981+ChenZhang1999@users.noreply.github.com>
2021-01-30 13:00:11 +0100

    Result part in Model

---
+++ GameOutcome.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+enum class GameOutcome {
+    WIN,
+    LOSE,
+    DRAW
+}
---
+++ Result.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+interface Result {
+    fun firstWins():Boolean
+    fun secondWins():Boolean
+    fun draws():Boolean
+}
